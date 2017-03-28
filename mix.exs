9d555d81702ca56eb184841ecd9d0b21881ae8db
Gabor Takacs <me@gabortakacs.com>
2017-03-28 12:39:35 +0200

    Package

--- mix.exs
+++ mix.exs
@@ -4,11 +4,13 @@ defmodule Golem.Mixfile do
   def project do
     [
       app: :golem,
-      version: "0.0.1",
+      version: "0.0.1.alpha1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      package: package(),
+      description: description(),
 
       test_coverage: [tool: Coverex.Task, coveralls: true],
 
@@ -16,11 +18,7 @@ defmodule Golem.Mixfile do
       name: "Golem",
       source_url: "https://github.com/ghaabor/golem",
       homepage: "https://github.com/ghaabor/golem",
-      docs: [
-        main: "Golem",
-        logo: "",
-        extras: ["README.md"]
-      ]
+      docs: docs()
     ]
   end
 
@@ -39,6 +37,31 @@ defmodule Golem.Mixfile do
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:credo, "~> 0.7", only: [:dev, :test]},
       {:coverex, "~> 1.4.10", only: :test}
+    ]
+  end
+
+  defp description do
+    """
+    Chatbot built in Elixir. BETA.
+    """
+  end
+
+  defp package do
+    [
+      name: :golem,
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Gábor Takács"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/ghaabor/golem",
+              "Docs" => "https://github.com/ghaabor/golem"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Golem",
+      logo: "",
+      extras: ["README.md"]
     ]
   end
 end
