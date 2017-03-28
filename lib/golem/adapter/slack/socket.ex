202cde10fe86a7e4e5d3713e7c78a4848d4d96a1
Gabor Takacs <me@gabortakacs.com>
2017-03-28 16:42:38 +0200

    Fix credo on slack socket

--- lib/golem/adapter/slack/socket.ex
+++ lib/golem/adapter/slack/socket.ex
@@ -2,8 +2,6 @@ defmodule Golem.Adapter.Slack.Socket do
   @moduledoc """
   Handles the Slack RTM socket connection.
   """
-  alias Golem.Adapter.Slack
-  alias Golem.Command
   alias Golem.CommandRegistry
 
   require Logger
@@ -14,7 +12,7 @@ defmodule Golem.Adapter.Slack.Socket do
     msg = type |> extract_message_for_type(resp)
 
     socket
-    |> handle_message(msg)
+    |> handle_message!(msg)
     |> recv!
   end
 
@@ -47,29 +45,41 @@ defmodule Golem.Adapter.Slack.Socket do
     end
   end
 
-  defp handle_message(socket, msg) do
-    case msg["type"] do
-      "ping" ->
-        socket |> pong!
-      "message" ->
-        socket |> find_and_execute_command(msg)
-      "reconnect_url" ->
-        :ets.insert(:config, {:reconnect_url, msg["url"]})
-        Logger.info("New reconnect_url: #{msg["url"]}")
-      "presence_change" ->
-        [{_id, user}] = :ets.lookup(:users, msg["user"])
-        Logger.info("@#{user["name"]} is now #{msg["presence"]}")
-      "user_typing" ->
-        [{_id, user}] = :ets.lookup(:users, msg["user"])
-        [{_id, channel}] = :ets.lookup(:channels, msg["channel"])
-        Logger.info("@#{user["name"]} is now typing in #{channel["name"] || user["name"]}")
-      "error" ->
-        Logger.error("#{msg["error"]["msg"]} (#{msg["error"]["code"]})")
-      _ ->
-        Logger.info("Unhandled message: [#{msg["type"] || "nil"}] #{Poison.encode!(msg)}")
-    end
-
+  defp handle_message!(socket, msg) do
+    handle_message_with_type(socket, msg["type"], msg)
     socket
+  end
+
+  defp handle_message_with_type(socket, type, _msg) when type == "ping" do
+    socket |> pong!
+  end
+
+  defp handle_message_with_type(socket, type, msg) when type == "message" do
+    socket |> find_and_execute_command(msg)
+  end
+
+  defp handle_message_with_type(_socket, type, msg) when type == "reconnect_url" do
+    :ets.insert(:config, {:reconnect_url, msg["url"]})
+    Logger.info("New reconnect_url: #{msg["url"]}")
+  end
+
+  defp handle_message_with_type(_socket, type, msg) when type == "presence_change" do
+    [{_id, user}] = :ets.lookup(:users, msg["user"])
+    Logger.info("@#{user["name"]} is now #{msg["presence"]}")
+  end
+
+  defp handle_message_with_type(_socket, type, msg) when type == "user_typing" do
+    [{_id, user}] = :ets.lookup(:users, msg["user"])
+    [{_id, channel}] = :ets.lookup(:channels, msg["channel"])
+    Logger.info("@#{user["name"]} is now typing in #{channel["name"] || user["name"]}")
+  end
+
+  defp handle_message_with_type(_socket, type, msg) when type == "error" do
+    Logger.error("#{msg["error"]["msg"]} (#{msg["error"]["code"]})")
+  end
+
+  defp handle_message_with_type(_socket, type, msg) do
+    Logger.info("Unhandled message: [#{type || "nil"}] #{Poison.encode!(msg)}")
   end
 
   defp find_and_execute_command(socket, msg) do
